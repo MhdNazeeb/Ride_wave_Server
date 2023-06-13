d0690abafeb6f1f03cc40e9bd40a867e3a15489f
muhammed naseeb <nasinaseeb13@gmail.com>
2023-06-13 21:59:42 +0530

    profile

--- controller/driver/driver.js
+++ controller/driver/driver.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const bcrypt = require("bcrypt");
 const cloudinary = require("../../utils/Cloudinary");
 const user = require("../../models/user");
-const Car = require('../../models/car')
+const Car = require("../../models/car");
 const jwt = require("jsonwebtoken");
+const { query } = require("express");
 
 const signup = async (req, res) => {
   try {
@@ -11,7 +12,7 @@ const signup = async (req, res) => {
     const findUser = await user.findOne({ email: email });
     console.log(findUser, "this find user");
     if (findUser && findUser.isDriver) {
-      console.log('this user');
+      console.log("this user");
       return res.json({ message: "email is all ready registred" });
     }
     if (password && cpassword) {
@@ -22,21 +23,23 @@ const signup = async (req, res) => {
       const file = await cloudinary.uploader.upload(license, {
         folder: "DriverLicense",
       });
-    if (findUser && !findUser.isDriver) {
-      console.log('this user');
+      if (findUser && !findUser.isDriver) {
+        console.log("this user");
+        await user.findOneAndUpdate(
+          { email: email },
+          {
+            name: fname,
+            password: hashedpassword,
+            confirm_password: hashedconfirmpassword,
+            license: file.secure_url,
+            isDriver: true,
+            DriverStatus: true,
+          }
+        );
+        return res.json({ message: "new account created sucessfully" });
+      }
+
       await user
-        .findOneAndUpdate({email: email },{
-          name: fname,
-          password: hashedpassword,
-          confirm_password: hashedconfirmpassword,
-          license: file.secure_url,
-          isDriver: true,
-          DriverStatus: true,
-        })
-      return res.json({ message: "new account created sucessfully" });
-    }
-    
-        await user
         .create({
           name: fname,
           email: email,
@@ -63,72 +66,116 @@ const signup = async (req, res) => {
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-  
-    const findDriver = await user.findOne({ email: email ,isDriver:true });
+
+    const findDriver = await user.findOne({ email: email, isDriver: true });
     if (!findDriver) return res.json({ status: "User doesn't exist" });
 
-    const isPasswordCorrect = await bcrypt.compare(password, findDriver.password);
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      findDriver.password
+    );
     console.log(isPasswordCorrect, "password");
     if (!isPasswordCorrect) {
       return res.json({ status: "incorrect password" });
     }
-      if(findDriver.isverify==='Rejected') {
-        await user.deleteOne(findDriver._id) 
-      return  res.json({status:'We regret to inform you that your account verification request has been rejected'})
-      }
-    if (findDriver.isverify === 'verified' && isPasswordCorrect) {
+    if (findDriver.isverify === "Rejected") {
+      await user.deleteOne(findDriver._id);
+      return res.json({
+        status:
+          "We regret to inform you that your account verification request has been rejected",
+      });
+    }
+    if (findDriver.isverify === "verified" && isPasswordCorrect) {
       if (findDriver.DriverStatus) {
         const toke = jwt.sign(
           { id: findDriver._id, role: "driver" },
           "ClientTokenSecret",
           { expiresIn: "5h" }
         );
-        res.status(200).json({ token: toke, driver: findDriver, status: "Login success" });
-      }else{
-       return res.status(200).json({status:'your account has been banned'});
+        res
+          .status(200)
+          .json({ token: toke, driver: findDriver, status: "Login success" });
+      } else {
+        return res.status(200).json({ status: "your account has been banned" });
       }
-      
-      }else{
-      return res.status(200).json({status:'it may take 24 houres to verify driver'})
-      }
-    }catch (error){
+    } else {
+      return res
+        .status(200)
+        .json({ status: "it may take 24 houres to verify driver" });
+    }
+  } catch (error) {
     console.log(error.message);
-    res.json({status:'something wrong'})
+    res.json({ status: "something wrong" });
+  }
+};
+const carRegister = async (req, res) => {
+  try {
+    console.log("this is rejister car");
+    const { model, year, RegistrationNumber, Features, Carimage, Rate } =
+      req.body;
+
+    const findcar = await Car.findOne({
+      RegistrationNumber: RegistrationNumber,
+    });
+    if (!findcar) {
+      const file = await cloudinary.uploader.upload(Carimage, {
+        folder: "carImage",
+      });
+      const car = await Car.create({
+        model: model,
+        year: year,
+        RegistrationNumber: RegistrationNumber,
+        Features: Features,
+        Rate: Rate,
+        carimage: file.secure_url,
+      });
+      console.log(car, "this is your car");
+      res
+        .status(200)
+        .json({ message: "it  may take 24 houres to verify your car" });
+    } else {
+      res.status(200).json({ message: "Registration number allready exists" });
     }
-     
-    };
-    const carRegister = async (req,res)=>{
-    try {
-      console.log('this is rejister car');
-      const { model, year,RegistrationNumber,Features,Carimage,Rate} = req.body
-      
-      const findcar = await Car.findOne({RegistrationNumber:RegistrationNumber})
-         if(!findcar){
-          const file = await cloudinary.uploader.upload(Carimage, {
-            folder: "carImage",
-          });
-          const car = await Car.create({
-            model:model,
-            year:year,
-            RegistrationNumber:RegistrationNumber,
-            Features:Features,
-            Rate:Rate,
-            carimage:file.secure_url
-          })
-          console.log(car,'this is your car');
-          res.status(200).json({message:'it  may take 24 houres to verify your car'})
-         }else{
-          res.status(200).json({message:'Registration number allready exists'})
-         }
-     
-    } catch (error) {
-      res.status(500).json({message:'something  wrong'})
-      console.log(error.message,);
-    }
-    }
+  } catch (error) {
+    res.status(500).json({ message: "something  wrong" });
+    console.log(error.message);
+  }
+};
+const profile = async (req, res) => {
+  try {
+    const { data} = req.body;
+    console.log(data,'thiis is data');
+    const findDriver = await user.findOneAndUpdate(
+      { _id: data.driverid },
+      {
+        $set: {
+          email: data.email,
+          name: data.name,
+        },
+      },
+      { new: true }
+    );
+    console.log(findDriver,'this is new drivr');
+    res.status(200).json({message:'success'})
+       
+  } catch (error) {
+    console.log(error.message,'this eroorrrrr');
+  }
+};
+const getProfile = async (req,res)=>{
+  try {
+    const {data} = req.query
+     const findUser = await user.findOne({_id:data})
+     res.status(200).json(findUser)
+  } catch (error) {
+    res.status(500)
+  }
+}
 
 module.exports = {
   signup,
   login,
-  carRegister
+  carRegister,
+  profile,
+  getProfile
 };
--- routes/driver.js
+++ routes/driver.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {signup,login, carRegister}=require('../controller/driver/driver')
+ const {signup,login, carRegister,profile,getProfile}=require('../controller/driver/driver')
 /* GET driver listing. */
 
  router.post('/signup',signup)
  router.post('/login',login)
  router.post('/car',carRegister)
+ router.patch('/profile',profile)
+ router.get('/profile',getProfile)
+
+
 module.exports = router;
