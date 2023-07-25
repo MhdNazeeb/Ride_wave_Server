2b8c60e38fc9a17c390541c0188f1d984fc16843
muhammed naseeb <nasinaseeb13@gmail.com>
2023-07-25 12:34:25 +0530

    card changed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const cors = require('cors')
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://ridewave-emd1.onrender.com',
   credentials: true,
   optionSuccessStatus: 200,
 };
